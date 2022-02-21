03f7f10d31b15703f11f52957c4652f3cf533bd3
Stiv1100001 <bichicchi.stefano@gmail.com>
2022-02-21 13:20:34 +0100

    First commit, end exercise

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const name = prompt('Inserisci il tuo nome');
+const lastname = prompt('Inserisci il tuo cognome');
+const color = prompt('Inserisci il tuo colore preferito');
+
+document.getElementById('password').innerHTML = name + lastname + color + '21';
